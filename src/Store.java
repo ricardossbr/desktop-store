c91042804debc65790675689ea2065714ec0be9c
Ricardo Soares <riicardo.soares@gmail.com>
2024-04-29 01:40:07 -0300

    Concluir o que foi pedido crud de produtos com listagem de acordo com o requisito.

--- src/Store.java
+++ src/Store.java
@@ -1,93 +1,24 @@
 package src;
 
-import java.io.File;
+import src.infra.DatabaseFile;
+import src.service.Display;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class Store {
     public static void main(String[] arg) {
-        showMenu();
-    }
-
-    private static void showMenu() {
-        System.out.println("1 - Listar produtos");
-        System.out.println("2 - Cadastrar novo produto");
-        System.out.println("3 - Editar produto");
-        System.out.println("4 - Excluir produto");
-        handleMenu();
-    }
-
-    private static void handleMenu() {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        switch (input) {
-            case "1":
-                getProduct();
-                break;
-            case "2":
-                salveProduct();
-                break;
-            case "3":
-                editProduct();
-                break;
-            case "4":
-                deleteProduct();
-                break;
-            default:
-
-        }
-    }
-
-    private static void getProduct() {
-        System.out.println("----GET PRODUCT-----");
-    }
-
-    private static void salveProduct() {
-        Scanner scanner = new Scanner(System.in);
-        Product product = new Product();
-
-        System.out.println("DIGITE O NOME DO PRODUTO");
-        product.name = scanner.nextLine();
-
-        System.out.println("DIGITE O VALOR DO PRODUTO");
-        try {
-            product.value = scanner.nextInt();
-        } catch (Exception e) {
-            e.getMessage();
-        }
-
-
-        System.out.println("DIGITE A QUANTIDADE DO PRODUTO");
-        try {
-            product.quantity = scanner.nextInt();
-        } catch (Exception e) {
-            e.getMessage();
-        }
-
-        saveInDataBase(product);
-
-        System.out.println("PRODUTO SALVO!");
-        showMenu();
-
+        Display display = new Display();
+        display.showMenu();
     }
 
 
-    private static void editProduct() {
-        System.out.println("----EDIT PRODUCT-----");
-    }
 
-    private static void deleteProduct() {
-        System.out.println("----DELETE PRODUCT-----");
-    }
 
-    private static void saveInDataBase(Product product) {
 
-    }
 
-    public static class Product {
-        protected String name;
-        protected int value;
-        protected int quantity;
-    }
+
+
 }
 
 
---
+++ src/service/Display.java
@@ -0,0 +1,133 @@
+package src.service;
+
+import src.Product;
+import src.infra.DatabaseFile;
+import src.infra.DatabaseImp;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Display {
+
+    private final Scanner scanner = new Scanner(System.in);
+    private final DatabaseImp databaseImp = new DatabaseImp();
+
+    private void handleMenu() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        switch (input) {
+            case "1":
+                getProduct();
+                showMenu();
+                break;
+            case "2":
+                salveProduct();
+                showMenu();
+                break;
+            case "3":
+                editProduct();
+                showMenu();
+                break;
+            case "4":
+                deleteProduct();
+                showMenu();
+                break;
+            case "exit":
+                break;
+            default:
+                showMenu();
+                break;
+
+        }
+    }
+
+    public void showMenu() {
+        System.out.println("1 - Listar produtos");
+        System.out.println("2 - Cadastrar novo produto");
+        System.out.println("3 - Editar produto");
+        System.out.println("4 - Excluir produto");
+        handleMenu();
+    }
+
+    private void getProduct() {
+        System.out.println("----GET PRODUCT-----");
+        List<Product> products = databaseImp.getProducts();
+        products.forEach(Product::print);
+    }
+
+    private void salveProduct() {
+
+        System.out.println("DIGITE O NOME DO PRODUTO");
+        String name = this.scanner.nextLine();
+
+        System.out.println("DIGITE O VALOR DO PRODUTO");
+        int value = 0;
+        try {
+             value = this.scanner.nextInt();
+        } catch (Exception e) {
+            e.getMessage();
+        }
+
+        System.out.println("DIGITE A QUANTIDADE DO PRODUTO");
+        int quantity = 0;
+        try {
+            quantity = this.scanner.nextInt();
+        } catch (Exception e) {
+            e.getMessage();
+        }
+
+        Product product = new Product(name, value, quantity);
+        databaseImp.saveProduct(product);
+
+        System.out.println("PRODUTO SALVO!");
+        showMenu();
+
+    }
+
+
+    private void editProduct() {
+        System.out.println("----EDIT PRODUCT-----");
+        System.out.println("----DIGITE O ID DO PRODUTO!-----");
+        int id = scanner.nextInt();
+        Product productFound = databaseImp.getById(id);
+        if(productFound != null) {
+            System.out.println("DIGITE O NOME DO PRODUTO");
+            String name = null;
+            while (true){
+                name = this.scanner.nextLine();
+                if(name == null || name.isEmpty()){
+                    name = this.scanner.nextLine();
+                }else{
+                    break;
+                }
+            }
+
+            System.out.println("DIGITE O VALOR DO PRODUTO");
+            int value = 0;
+            try {
+                value = this.scanner.nextInt();
+            } catch (Exception e) {
+                e.getMessage();
+            }
+
+            System.out.println("DIGITE A QUANTIDADE DO PRODUTO");
+            int quantity = 0;
+            try {
+                quantity = this.scanner.nextInt();
+            } catch (Exception e) {
+                e.getMessage();
+            }
+            productFound.prepareToEdit(name, value, quantity);
+            databaseImp.editProduct(productFound);
+            System.out.println("Produto editado com sucesso!");
+        }
+        System.out.println("id do produto não foi encontrado!");
+    }
+
+    private void deleteProduct() {
+        System.out.println("----DELETE PRODUCT-----");
+        int id = scanner.nextInt();
+        databaseImp.deleteProduct(id);
+    }
+
+}
